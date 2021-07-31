a6a4af738c642a77f9c74b2d08c70d1168e2eb5d
william-coulter <z5113817@ad.unsw.edu.au>
2021-07-31 20:15:48 +1000

    Deriving major injuries field

--- scripts/derive_new_fields.py
+++ scripts/derive_new_fields.py
@@ -1,11 +1,15 @@
 import pandas as pd
+import numpy as np
 
 from datetime import datetime
 
 ### CONTSTANTS ###
 
 PHYSICAL_INJURIES_PATH = "./data/processed/physical_injuries_2010_2021.csv"
-PLAYER_STATS_PATH = "./data/processed/player_stats_2010_2021.csv"
+PLAYER_STATS_PATH = "./data/cleaned/player_stats_2010_2021.csv"
+COMBINED_DATA_PATH = "./data/processed/combined.csv"
+
+MAJOR_INJURY_BOUNDARY = 34
 
 ### HELPER FUNCTIONS ###
 
@@ -14,10 +18,52 @@ def import_csv(path):
 
 ### SCRIPT START ###
 
-print("Deriving new fields...")
+print("\n\Script start...\n\n")
 
-physical_injuries = import_csv(PHYSICAL_INJURIES_PATH)
 player_stats = import_csv(PLAYER_STATS_PATH)
+physical_injuries = import_csv(PHYSICAL_INJURIES_PATH)
 
+print("Joining physical injury and player stat datasets...")
+# Set multiple indexes for each of these dataframes
+player_stats.set_index(
+    inplace=True,
+    keys=["Player", "Year", "Season"]
+)
+physical_injuries.set_index(
+    inplace=True,
+    keys=["Player", "Year", "Season"]
+)
 
+combined = player_stats.merge(
+    right=physical_injuries,
+    how="left",
+    on=["Player", "Year", "Season"]
+)
+# Clean out rows with identical values across all columns
+combined.drop_duplicates(inplace=True)
 
+# Derive 'Major Injury' column
+print("Deriving 'Major Injury' column...")
+for i, row in combined.iterrows():
+    # STARTHERE: Cannot set new value with multiple-indexes
+    print(i)
+    duration = row["Duration"]
+
+    # If "Duration" has a value and is greater than 34, then
+    # the player has suffered a major injury during that season
+    if not np.isnan(duration) and duration > MAJOR_INJURY_BOUNDARY:
+        combined.at[i, "Major Injury"] = "1"
+    else:
+        combined.at[i, "Major Injury"] = "0"
+
+combined.to_csv(COMBINED_DATA_PATH)
+
+# Derive 'Recent Minor Injury Count' column
+print("Deriving 'Recent Minor Injury Count' column...")
+# STARTHERE: I think it's:
+#
+# combined.groupby(by=["Hmmm this might need to be a shit-load of columns"], dropna=False).size()
+# in "by", need to include "major injury" column
+
+# Derive 'Previous Major Injury Count' column
+print("Deriving 'Previous Major Injury Count' column...")
--- scripts/process_injury_list.py
+++ scripts/process_injury_list.py
@@ -154,158 +154,6 @@ print(f"Number of injuries relating to COVID-19: {covid_entries.shape[0]}")
 print(f"Number of injuries relating to personal reasons: {personal_entries.shape[0]}")
 print(f"Remaining injury related entries: {il_injuries.shape[0]}")
 
-il_injuries.to_csv("./data/processed/physical_injuries_2010_2021.csv")
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
-
-import math
-from datetime import datetime
-
-def import_csv(path):
-    return pd.read_csv(path, index_col=0)
-
-def is_acquired_entry(row):
-    # Empty entries import as "NaN" which is of type float
-    return isinstance(row["Relinquished"], float)
-
-print("\n\nProcessing injury list...\n\n")
-
-# Import cleaned IL
-full_IL = import_csv("./data/cleaned/injury_list_2010_2021.csv")
-
-# For each "relinquished" entry, find the corresponding "acquired"
-# entry and add this to the empty "Acquired" column
-print("Fixing 'Acquired' column, this may take a while...")
-for i, row in full_IL.iterrows():
-    # Skip row if its an "acquired" entry
-    if is_acquired_entry(row):
-        continue
-
-    player = row["Relinquished"]
-
-    # Iterate over remaining dataframe and find the next "acquired" entry
-    # Flag to track whether the return date was found
-    found = False
-    for i2, row2 in full_IL[i+1:].iterrows():
-        if found == True:
-            break
-
-        # Assume that no players have the same name
-        if row2["Acquired"] == player or row2["Relinquished"] == player:
-
-            if is_acquired_entry(row2):
-                # This entry contains when the player returned from their injury
-                full_IL.at[i, "Acquired"] = row2["Date"]
-                found = True
-
-            else:
-                # This entry is ANOTHER injury which means they were out
-                # for the season due to their last injury
-                #
-                # Let's mark this injury as season
-                full_IL.at[i, "Acquired"] = "season"
-                found = True
-
-    # If the return date was not found, then the player has not returned
-    if found == False:
-        full_IL.at[i, "Acquired"] = "season"
-
-# Filter entries regarding when a player left the IL
-il_relinquished = full_IL.dropna(subset=["Relinquished"])
-print(f"Number of relinquished entries: {il_relinquished.shape[0]}")
-
-# Filter out non-injury related IL items
-illness_entries = il_relinquished[(il_relinquished['Notes'].str.contains('illness')) | (il_relinquished['Notes'].str.contains('flu'))]
-surgery_entries = il_relinquished[il_relinquished['Notes'].str.contains('surgery')]
-covid_entries = il_relinquished[il_relinquished['Notes'].str.contains('COVID-19')]
-personal_entries = il_relinquished[il_relinquished['Notes'].str.contains('personal')]
-
-non_injuries = pd.concat(objs=[illness_entries, surgery_entries, covid_entries, personal_entries])
-il_injuries = il_relinquished.merge(non_injuries, how="outer", indicator=True).loc[lambda x : x['_merge']=='left_only'].drop(columns=['_merge'])
-
-print(f"Number of injuries relating to illness: {illness_entries.shape[0]}")
-print(f"Number of injuries relating to surgery: {surgery_entries.shape[0]}")
-print(f"Number of injuries relating to COVID-19: {covid_entries.shape[0]}")
-print(f"Number of injuries relating to personal reasons: {personal_entries.shape[0]}")
-print(f"Remaining injury related entries: {il_injuries.shape[0]}")
-
-il_injuries.to_csv("./data/processed/physical_injuries_2010_2021.csv")
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
-
-import math
-from datetime import datetime
-
-def import_csv(path):
-    return pd.read_csv(path, index_col=0)
-
-def is_acquired_entry(row):
-    # Empty entries import as "NaN" which is of type float
-    return isinstance(row["Relinquished"], float)
-
-print("\n\nProcessing injury list...\n\n")
-
-# Import cleaned IL
-full_IL = import_csv("./data/cleaned/injury_list_2010_2021.csv")
-
-# For each "relinquished" entry, find the corresponding "acquired"
-# entry and add this to the empty "Acquired" column
-print("Fixing 'Acquired' column, this may take a while...")
-for i, row in full_IL.iterrows():
-    # Skip row if its an "acquired" entry
-    if is_acquired_entry(row):
-        continue
-
-    player = row["Relinquished"]
-
-    # Iterate over remaining dataframe and find the next "acquired" entry
-    # Flag to track whether the return date was found
-    found = False
-    for i2, row2 in full_IL[i+1:].iterrows():
-        if found == True:
-            break
-
-        # Assume that no players have the same name
-        if row2["Acquired"] == player or row2["Relinquished"] == player:
-
-            if is_acquired_entry(row2):
-                # This entry contains when the player returned from their injury
-                full_IL.at[i, "Acquired"] = row2["Date"]
-                found = True
-
-            else:
-                # This entry is ANOTHER injury which means they were out
-                # for the season due to their last injury
-                #
-                # Let's mark this injury as season
-                full_IL.at[i, "Acquired"] = "season"
-                found = True
-
-    # If the return date was not found, then the player has not returned
-    if found == False:
-        full_IL.at[i, "Acquired"] = "season"
-
-# Filter entries regarding when a player left the IL
-il_relinquished = full_IL.dropna(subset=["Relinquished"])
-print(f"Number of relinquished entries: {il_relinquished.shape[0]}")
-
-# Filter out non-injury related IL items
-illness_entries = il_relinquished[(il_relinquished['Notes'].str.contains('illness')) | (il_relinquished['Notes'].str.contains('flu'))]
-surgery_entries = il_relinquished[il_relinquished['Notes'].str.contains('surgery')]
-covid_entries = il_relinquished[il_relinquished['Notes'].str.contains('COVID-19')]
-personal_entries = il_relinquished[il_relinquished['Notes'].str.contains('personal')]
-
-non_injuries = pd.concat(objs=[illness_entries, surgery_entries, covid_entries, personal_entries])
-il_injuries = il_relinquished.merge(non_injuries, how="outer", indicator=True).loc[lambda x : x['_merge']=='left_only'].drop(columns=['_merge'])
-
-print(f"Number of injuries relating to illness: {illness_entries.shape[0]}")
-print(f"Number of injuries relating to surgery: {surgery_entries.shape[0]}")
-print(f"Number of injuries relating to COVID-19: {covid_entries.shape[0]}")
-print(f"Number of injuries relating to personal reasons: {personal_entries.shape[0]}")
-print(f"Remaining injury related entries: {il_injuries.shape[0]}")
-
 # Calculate days that injury left player on injury list and add the year and season
 for i, row in il_injuries.iterrows():
     il_injuries.at[i, "Duration"] = calculate_days_on_il(row, 100)
@@ -314,6 +162,13 @@ for i, row in il_injuries.iterrows():
         il_injuries.at[i, "Season"] = "regular"
     else:
         il_injuries.at[i, "Season"] = "post"
+
+# Rename:
+#   "Relinquished" -> "Player"
+il_injuries.rename(
+    inplace=True,
+    columns={"Relinquished": "Player"}
+)
 
 # Save dataframe
 il_injuries.to_csv("./data/processed/physical_injuries_2010_2021.csv")
